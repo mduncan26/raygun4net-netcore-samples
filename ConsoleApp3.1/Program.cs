0f491b6b1b5f380c70a3dc562baf7a4087455168
Mitchell <mitchell@raygun.com>
2020-11-05 10:29:09 +1300

    Adding console sample app for .NET Core 3.1
    Adding console sample app for .NET Core 3.1

---
+++ ConsoleApp3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Mindscape.Raygun4Net;
+
+namespace ConsoleApp3_1
+{
+  class Program
+  {
+    private static RaygunClient _raygunClient = new RaygunClient("YOUR_APP_API_KEY");
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      try
+      {
+        // Do something here that might go wrong
+        throw new Exception("Hello Raygun!");
+      }
+      catch (Exception e)
+      {
+        _raygunClient.Send(e);
+      }
+    }
+  }
+}
